417119d1531b07015f45b139e18c6b08b2bca4a1
Javier Castro <javiercastro.ik@gmail.com>
2017-02-25 00:09:39 -0500

    Lista de usuarios terminado

--- source/javascript/app.js
+++ source/javascript/app.js
@@ -1,3 +1,1 @@
 var userList = require('./libs/list_users.js');
-
-var usuariosFiltro = new userList();
--- source/javascript/libs/list_users.js
+++ source/javascript/libs/list_users.js
@@ -1,8 +1,15 @@
-var $api_URL = 'https://randomuser.me/api/?results=10';
+var $api_URL = 'https://randomuser.me/api/?results=20';
+
+Vue.filter('capitalize', function (value) {
+  if (!value) return ''
+  value = value.toString()
+  return value.charAt(0).toUpperCase() + value.slice(1)
+})
 
 var app = new Vue({
   el: '#app',
   data: {
+    search: '',
     personas: []
   },
   created: function(){
@@ -16,8 +23,16 @@ var app = new Vue({
             console.log(response);
           });
     }
+  },
+  computed: {
+    filteredItems() {
+      return this.personas.filter(user => {
+        var nameComplete = user.name.first +" "+ user.name.last
+        return nameComplete.indexOf(this.search.toLowerCase()) > -1
+      })
+    }
   }
 })
 
 //Si queremos mostrar el json en el HTML
-Vue.filter('json', value => { return JSON.stringify(value, null, 2) } )
+// Vue.filter('json', value => { return JSON.stringify(value, null, 2) } )
